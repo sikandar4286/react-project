73cb1fe641f6b06e1f5f8ad387907977076a5ffe
Sikandar Ali <sikandarali4286@gmail.com>
2020-11-17 18:11:14 +0500

    Initial project version

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    // BEN
+    <div className="app">
+      <h1>hello sikandar ali basharat!!!!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC8wUaaidvlk6cOvQTUzMXCnDC66Yl_DBA",
+  authDomain: "challenge-2349d.firebaseapp.com",
+  databaseURL: "https://challenge-2349d.firebaseio.com",
+  projectId: "challenge-2349d",
+  storageBucket: "challenge-2349d.appspot.com",
+  messagingSenderId: "16039248062",
+  appId: "1:16039248062:web:46f6db9adecdd2b545deef"
+};
