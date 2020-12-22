99456239974356cb4956755737e945993892d5f2
Sikandar Ali <sikandarali4286@gmail.com>
2020-12-22 20:54:34 +0500

    add StateProvider.js reducer.js to the project and modify other file in the project

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,12 @@ import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+    const [{ basket }, dispatch] = useStateValue();
+    console.log(basket.lenght);
+
     return (
         <div className="header">
             <Link to="./">
@@ -31,7 +35,9 @@ function Header() {
                 <Link to="./Checkout">
                     <div className="header__optionBasket">
                         <ShoppingBasketIcon className="header__ShoppingBasketIcon"></ShoppingBasketIcon>
-                        <span className="header__optionLineTwo header__basketCount">0</span>
+                        <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
+                        {/* there is a spel mistake dude wrong{lenght}, corect{length}*/}
+                        {/* <span className="header__optionLineTwo header__basketCount">{basket.lenght}</span> */}
                     </div>
                 </Link>
             </div>
--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,24 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
-function Product({ title, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
+    const [{ basket }, dispatch] = useStateValue();
+    console.log("The is the basket >>>>>>", basket);
+    const addToBasket = () => {
+        // dispatch the items into the data layer
+        dispatch({
+            type: "ADD_TO_BASKET",
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
+    };
+
     return (
         <div className="product">
             <div className="product__info">
@@ -20,7 +37,7 @@ function Product({ title, image, price, rating }) {
             </div>
             <img src={image} alt="hello world"></img>
             {/* <img src="https://www.junglescout.com/wp-content/uploads/2020/03/Screen-Shot-2020-05-15-at-1.00.40-PM.png" alt="hello world"></img> */}
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     );
 }
